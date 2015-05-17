347c569e90f74e5f5c6545dc1905f5643f5af753
jykke <jyri.lehva@saunalahti.fi>
2015-05-17 20:40:48 +0300

    auto-indent lines

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
-  // route to handle all angular requests
-  app.get('*', function(req, res) {
-      res.sendfile('./public/views/index.html'); // load our public/index.html file
-  });
+    // route to handle all angular requests
+    app.get('*', function(req, res) {
+        res.sendfile('./public/views/index.html'); // load our public/index.html file
+    });
 }
