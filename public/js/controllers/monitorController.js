a890953c05ef6b188c055e6726622c4663662b61
jykke <jyri.lehva@saunalahti.fi>
2015-05-24 22:57:53 +0300

    added latest temperature and humidity to the dashboard

--- public/js/controllers/monitorController.js
+++ public/js/controllers/monitorController.js
@@ -2,4 +2,16 @@ angular.module('app').controller("MonitorController", function($scope, FirebaseS
     var vm = this; // vm == ViewModel
     vm.hello = "hello world!";
     vm.statistics = FirebaseService.getStatistics();
+
+    vm.latestTemperature = function() {
+        return vm.statistics[vm.statistics.length - 1].temperature;
+    }
+
+    vm.latestHumidity = function() {
+        return vm.statistics[vm.statistics.length - 1].humidity;
+    }
+
+    vm.updatedAt = function() {
+        return vm.statistics[vm.statistics.length - 1].time;
+    }
 });
--- public/js/services/FirebaseService.js
+++ public/js/services/FirebaseService.js
@@ -1,6 +1,6 @@
 angular.module('app').service('FirebaseService', function($firebaseArray) {
     var baseUrl = "https://luminous-fire-393.firebaseio.com";
-    var ref = new Firebase(baseUrl + "/temperature");
+    var ref = new Firebase(baseUrl + "/humiditytemperature");
 
     this.getStatistics = function() {
         return $firebaseArray(ref);
