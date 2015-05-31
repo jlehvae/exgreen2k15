52cce967d033a6d277944d7e3b5fddaf3dbfe4cd
jykke <jyri.lehva@saunalahti.fi>
2015-05-31 13:39:47 +0300

    percentage circles for latest metric values

--- public/js/controllers/monitorController.js
+++ public/js/controllers/monitorController.js
@@ -7,6 +7,8 @@ angular.module('app').controller("MonitorController", function ($scope, Firebase
 
         vm.latestTemperature = vm.statistics[latest].temperature;
         vm.latestHumidity = vm.statistics[latest].humidity;
+        // remove the dot for the graph
+        vm.graphHumidity = vm.latestHumidity.toString().substring(0, vm.latestHumidity.toString().indexOf("."));
         vm.updatedAt = vm.statistics[latest].time;
         vm.latestSoilmoisture = vm.statistics[latest].soil;
     });
