d8e1f3202feeb6bbd20807c81d3feaf930a7e616
jykke <jyri.lehva@saunalahti.fi>
2015-05-31 11:30:31 +0300

    random changes to soil moisture

--- public/js/services/SoilDataService.js
+++ public/js/services/SoilDataService.js
@@ -10,8 +10,12 @@ angular.module('app').service('SoilDataService', function(FirebaseService) {
                 bottom: 40,
                 left: 55
             },
-            x: function(d){ return d.x; },
-            y: function(d){ return d.y; },
+            x: function(d){
+                return d.x;
+            },
+            y: function(d){
+                return d.y;
+            },
             useInteractiveGuideline: true,
             dispatch: {
                 stateChange: function(e){ console.log("stateChange"); },
@@ -21,6 +25,9 @@ angular.module('app').service('SoilDataService', function(FirebaseService) {
             },
             xAxis: {
                 axisLabel: 'Time',
+                tickFormat: function(d) {
+                    return d3.time.format('%H:%M')(new Date(d))
+                }
             },
             yAxis: {
                 axisLabel: 'Soil moisture',
@@ -30,16 +37,16 @@ angular.module('app').service('SoilDataService', function(FirebaseService) {
                 axisLabelDistance: 30
             },
             callback: function(chart){
-                console.log("!!! lineChart callback !!!");
+                //console.log("!!! lineChart callback !!!");
             }
         },
         title: {
             enable: true,
-            text: 'Title for Line Chart'
+            text: 'Soil moisture'
         },
         subtitle: {
             enable: false,
-            text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+            text: '',
             css: {
                 'text-align': 'center',
                 'margin': '10px 13px 0px 7px'
@@ -47,7 +54,7 @@ angular.module('app').service('SoilDataService', function(FirebaseService) {
         },
         caption: {
             enable: false,
-            html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+            html: '',
             css: {
                 'text-align': 'justify',
                 'margin': '10px 13px 0px 7px'
