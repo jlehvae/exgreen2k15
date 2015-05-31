e03490d2a6c3d5f2e6b272cf207ea4fe318a7777
jykke <jyri.lehva@saunalahti.fi>
2015-05-31 12:43:49 +0300

    refactoring

--- public/js/controllers/monitorController.js
+++ public/js/controllers/monitorController.js
@@ -1,23 +1,13 @@
 angular.module('app').controller("MonitorController", function ($scope, FirebaseService, SoilDataService) {
     var vm = this; // vm == ViewModel
-    vm.hello = "hello world!";
     vm.statistics = FirebaseService.getStatistics();
 
-    vm.latestTemperature = function () {
-        return vm.statistics[vm.statistics.length - 1].temperature;
-    }
-
-    vm.latestHumidity = function () {
-        return vm.statistics[vm.statistics.length - 1].humidity;
-    }
-
-    vm.updatedAt = function () {
-        return vm.statistics[vm.statistics.length - 1].time;
-    }
-
-    vm.latestSoilmoisture = function () {
-        return vm.statistics[vm.statistics.length - 1].soil;
-    }
+    vm.statistics.$loaded().then(function () {
+        vm.latestTemperature = vm.statistics[vm.statistics.length - 1].temperature;
+        vm.latestHumidity = vm.statistics[vm.statistics.length - 1].humidity;
+        vm.updatedAt = vm.statistics[vm.statistics.length - 1].time;
+        vm.latestSoilmoisture = vm.statistics[vm.statistics.length - 1].soil;
+    });
 
     vm.soilData = SoilDataService.getSoilData();
     vm.soilOptions = SoilDataService.options;
