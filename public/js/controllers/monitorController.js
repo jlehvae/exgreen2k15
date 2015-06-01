b1d815358175a4b2c330578670c3df0afc646b87
jykke <jyri.lehva@saunalahti.fi>
2015-06-01 11:38:54 +0300

    data now changes on realtime on the page and moisture shows correct percentage value

--- public/js/controllers/monitorController.js
+++ public/js/controllers/monitorController.js
@@ -3,18 +3,39 @@ angular.module('app').controller("MonitorController", function ($scope, Firebase
     vm.statistics = FirebaseService.getStatistics();
 
     vm.statistics.$loaded().then(function () {
+        updateScope();
+        vm.statistics.$watch(function() {
+            updateScope();
+        });
+    });
+
+    function updateScope() {
         var latest = vm.statistics.length - 1;
+
+        vm.updatedAt = vm.statistics[latest].time;
 
         vm.latestTemperature = vm.statistics[latest].temperature;
         vm.latestHumidity = vm.statistics[latest].humidity;
-        // remove the dot for the graph
-        vm.graphHumidity = vm.latestHumidity.toString().substring(0, vm.latestHumidity.toString().indexOf("."));
-        vm.updatedAt = vm.statistics[latest].time;
         vm.latestSoilmoisture = vm.statistics[latest].soil;
-    });
 
-    vm.soilData = SoilDataService.getSoilData();
-    vm.soilOptions = SoilDataService.options;
+        vm.graphHumidity = removeDot(vm.latestHumidity.toString());
+        vm.graphTemperature = removeDot(vm.latestTemperature.toString());
+        vm.graphMoisture = convertSoilMetricToPercentage(vm.latestSoilmoisture);
 
+        vm.soilData = SoilDataService.getSoilData();
+        vm.soilOptions = SoilDataService.options;
+    }
 
+    function removeDot(stringValue) {
+        if(stringValue.indexOf('.') > -1) {
+            return stringValue.substring(0, stringValue.toString().indexOf('.'));
+        } else {
+            return stringValue;
+        }
+    }
+
+    function convertSoilMetricToPercentage(number) {
+        var percentage = (number / 700) * 100;
+        return removeDot(percentage.toString());
+    }
 });
